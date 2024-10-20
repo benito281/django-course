e602f884e0aca6210608b9b08555a7cfeec01704
benito281 <benitoortiz281@gmail.com>
2024-10-19 22:44:04 -0300

    Model Actualizado

--- myapp/models.py
+++ myapp/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.utils import timezone
+import uuid
 
 # Create your models here.
 class Projects(models.Model):
-    name_project = models.CharField(max_length=150)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+    name_project = models.CharField(max_length=100)
+    description = models.CharField(max_length=255, default=timezone.now)
 
     def __str__(self):
         return self.name_project
@@ -13,6 +17,8 @@ class Task(models.Model):
     description = models.CharField(max_length=200) 
     project = models.ForeignKey(Projects, on_delete=models.CASCADE)
     done = models.BooleanField(default=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) 
+
 
     def __str__(self):
         return self.title + ' - ' + self.project.name_project
